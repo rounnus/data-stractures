f2832487e16d090d84c65b83f1dfb718238dd3e5
Constantinos Argyriou <rounnus@gmail.com>
2022-05-18 17:33:20 +0300

    done

--- src/question2/main.cpp
+++ src/question2/main.cpp
@@ -29,14 +29,51 @@ static void fill_heaps_with_random(float max[], float min[],
 }
 
 
-static inline void add_float_using_max()
+static void add_float_using_max(ds_ex::Max_Heap<float>& max_h)
 {
 
+	float f1;
+	float f2;
 
+	float sum;
+	while (max_h.size() != 0)
+	{
+		// retrieve the first num to add.
+		max_h.delete_max(f1);
+		
+		if (max_h.size() == 0) break;
+		// retrieve the second num to add.
+		max_h.delete_max(f2);
+	
+		// add the numbers.
+		sum = f1 + f2;
+		max_h.insert_element(sum);
+	}
+
+	std::cout << "The result of adding floats using max heap is: " << f1 << std::endl;
 }
 
-static inline void add_float_using_min()
+static inline void add_float_using_min(ds_ex::Min_Heap<float>& min_h)
 {
+	float f1;
+	float f2;
+
+	float sum;
+	while (min_h.size() != 0)
+	{
+		// retrieve the first num to add.
+		min_h.delete_min(f1);
+
+		if (min_h.size() == 0) break;
+		// retrieve the second num to add.
+		min_h.delete_min(f2);
+	
+		// add the numbers.
+		sum = f1 + f2;
+		min_h.insert_element(sum);
+	}
+
+	std::cout << "The result of adding floats using min heap is: " << f1 << std::endl;
 
 
 }
@@ -64,6 +101,8 @@ int main()
 	max_heap.initialize(max_array, n, n);
 	min_heap.initialize(min_array, n, n);
 
+	add_float_using_min(min_heap);
+	add_float_using_max(max_heap);
 
 	return 0;
 }
